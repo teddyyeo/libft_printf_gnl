3921c1728e15ee4c41b1f5e284ef2dbbf23edae5
tayeo <tayeo@student.42seoul.kr>
2022-10-16 05:30:38 +0900

    all files

---
+++ basic_utils.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   basic_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayeo <tayeo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 15:33:55 by tayeo             #+#    #+#             */
+/*   Updated: 2022/10/15 21:37:54 by tayeo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+int	ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int	ft_putstr(char *s)
+{
+	if (!s)
+		return (0);
+	return (write(1, s, ft_strlen(s)));
+}
+
+size_t	ft_strlen(const char *s)
+{
+	const char	*start;
+
+	start = s;
+	while (*s)
+		s++;
+	return (s - start);
+}
